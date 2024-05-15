d2746cb425a0107237367f14d89fb58a9e1d86c7
guotingjin <dev@peercat.cn>
2024-05-15 13:46:41 +0800

    更新文档

--- docs/src/components/DemoEditor.vue
+++ docs/src/components/DemoEditor.vue
@@ -1,6 +1,6 @@
 <template>
     <button @click="handleRun" class="runButton" style="margin-bottom: 12px;">在浏览器中运行(请自行打开控制台)</button>
-    <div ref="code" :style="{ width: '100%', height: height }"></div>
+    <div ref="codeEditor" :style="{ width: '100%', height: height }"></div>
 </template>
 
 <style>
@@ -19,81 +19,81 @@
 }
 </style>
 
-<script>
-import { defineComponent, ref } from 'vue';
+<script setup>
+import { ref, onMounted, getCurrentInstance, watch } from 'vue';
 import { runLog4aCode } from '@peercat/log4a';
 import { useData } from 'vitepress';
 import * as monaco from 'monaco-editor';
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
-
-let keywords = [
-    'const',
-    'struct',
-    'abstract',
-    'this',
-    'module',
-    'export',
-    'interface',
-    'let',
-    'type',
-    'function',
-    'return',
-    'class',
-    'public',
-    'private',
-    'constructor',
-    'implements',
-    'new',
-    'static',
-    'for', 'of', 'in', 'continue', 'typeof', 'if', 'else', 'while', 'LogManager', 'extends'
-];
-self.MonacoEnvironment = {
-    getWorker(_, __) {
-        if (__ == 'typescript' || __ == 'javascript') {
-            return new tsWorker();
-        }
-        return new editorWorker()
-    }
+const props = defineProps({
+    code: String,
+    height: Number
+});
+const { isDark } = useData();
+const code = ref(props.code ?? '');
+const height = ref(props.height ?? '500px');
+const codeEditor = ref('codeEditor');
+const handleRun = () => {
+    runLog4aCode(code.value);
 }
-export default defineComponent({
-    data() {
-        const cp = this.$attrs.code ?? '';
-        const code = ref(cp);
-        const h = this.$attrs.height ?? '500px'
-        return {
-            handleRun: () => {
-                runLog4aCode(code.value);
-            },
-            code,
-            editor: null,
-            height: h
-        }
-    },
-    mounted() {
-        this.initEditor();
-    },
-    methods: {
-        initEditor() {
-            const { isDark } = useData();
-            const editor = monaco.editor.create(this.$refs.code, {
-                theme: isDark ? 'vs-dark' : 'vs',
-                value: this.code,
-                language: 'javascript',
-                lineNumbers: 'on',
-                folding: true,
-                scrollBeyondLastLine: true,
-                automaticLayout: true,
-                codeLens: false,
-                minimap: {
-                    enabled: false
-                }
-            });
-            editor.onDidChangeModelContent(() => {
-                const v = editor.getValue();
-                this.code = v;
-            })
+
+const initializeEditor = (el) => {
+    let keywords = [
+        'const',
+        'struct',
+        'abstract',
+        'this',
+        'module',
+        'export',
+        'interface',
+        'let',
+        'type',
+        'function',
+        'return',
+        'class',
+        'public',
+        'private',
+        'constructor',
+        'implements',
+        'new',
+        'static',
+        'for', 'of', 'in', 'continue', 'typeof', 'if', 'else', 'while', 'LogManager', 'extends'
+    ];
+    self.MonacoEnvironment = {
+        getWorker(_, __) {
+            if (__ == 'typescript' || __ == 'javascript') {
+                return new tsWorker();
+            }
+            return new editorWorker()
         }
     }
+    const editor = monaco.editor.create(el, {
+        theme: isDark ? 'vs-dark' : 'vs',
+        value: code.value,
+        language: 'javascript',
+        lineNumbers: 'on',
+        folding: true,
+        scrollBeyondLastLine: true,
+        automaticLayout: true,
+        codeLens: false,
+        minimap: {
+            enabled: false
+        }
+    });
+    editor.onDidChangeModelContent(() => {
+        const v = editor.getValue();
+        code.value = v;
+    })
+    watch(isDark, (v) => {
+        editor.updateOptions({
+            theme: v ? 'vs-dark' : 'vs'
+        })
+    })
+}
+onMounted(() => {
+    const instance = getCurrentInstance();
+    if (instance)
+        initializeEditor(instance.refs.codeEditor);
 })
 </script>
